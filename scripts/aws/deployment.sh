49a46d4dadfd1aa09d5779ec03d77b0bdc049742
yawnak <shliakhov.danylo@gmail.com>
2023-10-15 16:47:21 +0300

    feat: add deployment script for aws

---
+++ scripts/aws/deployment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Update the system and install Docker
+sudo yum update -y
+sudo yum install docker -y
+sudo service docker start
+
+# Pull the Docker image
+sudo docker pull yawnak/hello-world:v1.0.0
+
+# Run the Docker container and map port 3000 on the host to port 80 in the container
+sudo docker run -d -p 3000:80 yawnak/hello-world:v1.0.0
