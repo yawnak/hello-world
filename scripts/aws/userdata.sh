4b32086476c67feaf27acaabef352931ba5fe73c
yawnak <shliakhov.danylo@gmail.com>
2023-10-15 17:29:44 +0300

    fix: wrong image name

--- scripts/aws/userdata.sh
+++ scripts/aws/userdata.sh
@@ -5,7 +5,7 @@ sudo yum install docker -y
 sudo service docker start
 
 # Pull the Docker image
-sudo docker pull yawnak/hello-world:v1.0.0
+sudo docker pull yawnak/helloworld:v1.0.0
 
 # Run the Docker container and map port 3000 on the host to port 80 in the container
-sudo docker run -d -p 3000:3000 yawnak/hello-world:v1.0.0
+sudo docker run -d -p 3000:3000 yawnak/helloworld:v1.0.0
